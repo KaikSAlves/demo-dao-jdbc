2c29ac69c9a62c7ea3a9d75c33f5f000e1eb9428
KaikSAlves <souzaalveskaik@gmail.com>
2025-01-06 07:58:01 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,19 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        Department d = new Department(1, "books");
+
+        Seller s = new Seller(12,"Kaik", "kaik@gmail.com", new Date(), 3000.0, d);
+
+        System.out.println(s);
     }
 }
